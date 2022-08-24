75d86d665bb17daf8f13d26c58d416008c323aa3
Ivan <ivanfrasca92@gmail.com>
2022-08-24 11:38:27 +0200

    props-05 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Age } from "./Age";
 
-export class Welcome extends React.Component{
-    render(){
-        return (
-          <div>
-            <p>Welcome, {this.props.name}!</p>
-            <p>Your Age is {this.props.age}</p>
-          </div>
-        );
-    }
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+        <p>
+          <Age age={this.props.age}/>
+        </p>
+      </div>
+    );
+  }
 }
 
 Welcome.defaultProps = {
-    name: "Pippo"
-}
+  name: "Pippo",
+};
