0967b570290b564c4c0979c4dc7c4b973f8f053e
Ivan <ivanfrasca92@gmail.com>
2022-08-26 12:19:41 +0200

    components-07-fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Message } from "./Message";
-
-export class Hello extends React.Component{
-    render(){
-        return (
-        <h1> Hello, World! </h1>
-        )
-    }
-}
+import { Hello } from "./Hello";
 
 export class App extends React.Component{
     render(){
