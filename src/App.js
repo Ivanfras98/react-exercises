2b69fb24c498ed138e56810166369738bd53e779
Ivan <ivanfrasca92@gmail.com>
2022-08-26 12:21:32 +0200

    fix es-component-06

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { Message } from "./Message";
-
-export class Hello extends React.Component{
-    render(){
-        return (
-        <h1> Hello, World! </h1>
-        )
-    }
-}
+import { Hello } from "./Hello";
 
 export class App extends React.Component{
     render(){
         return (
           <div>
             <Hello />
-            <Message />
+            <p>What a beautiful day!</p>
           </div>
         );
     }
