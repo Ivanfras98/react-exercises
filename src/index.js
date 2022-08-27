68454666298729e153ae464a32f2aa751e065647
Ivan <ivanfrasca92@gmail.com>
2022-08-27 20:49:59 +0200

    es-style-01-done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { App } from "./App";
 import ReactDOM  from "react-dom";
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 
