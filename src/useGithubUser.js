4303be70353a2792002aefe956d1cc1f91474339
Ivan <ivanfrasca92@gmail.com>
2022-09-03 11:53:53 +0200

    custom-hooks-04-done

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -7,15 +7,33 @@ export function useGithubUser(username) {
     followers: null,
     url: null,
   });
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState("");
+
   async function getApi() {
-    const fetchedApi = await fetch(`${API}${username}`);
-    const data = await fetchedApi.json();
-    setDataApi(data);
+    setLoading(true);
+    setError(null);
+    try {
+      const fetchedApi = await fetch(`${API}${username}`);
+      const data = await fetchedApi.json();
+      if (fetchedApi.status !== 200) {
+        setError(new Error());
+      }
+      setDataApi(data);
+    } catch (error) {
+      setError(error);
+      setDataApi.name(null);
+    } finally {
+      setLoading(false);
+    }
   }
   useEffect(() => {
     getApi(username);
   }, [username]);
   return {
+    getApi,
     dataApi,
+    loading,
+    error
   };
 }
