e3d2230c8cda48fb81783ea23ba4e342822147c2
Ivan <ivanfrasca92@gmail.com>
2022-09-06 12:56:55 +0200

    es-swr-01-done

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,18 +2,15 @@ import { useEffect, useState } from "react"
 import { useGithubUser } from "./useGithubUser"
 
 export function GithubUser({username}){
-    const {dataApi, error, loading, getApi} = useGithubUser(username)
-    function HandleDataFetch(){
-        getApi(username)
-    }
+    const {data, error, fetcher} = useGithubUser(username)
     return (
       <div style={{ color: "green" }}>
-        <button onClick={HandleDataFetch}>Load user</button>
-        {loading && <h1>Loading..</h1>}
-        {error && <h1>There was an error</h1>}
-        {dataApi.name && <h1>Nome dell'utente : {dataApi.name}</h1>}
-        {dataApi.followers && <h3>Followers : {dataApi.followers}</h3>}
-        {dataApi.url && <h3>Url GitHub{dataApi.url}</h3>}
+        {/* <button onClick={HandleDataFetch}>Load user</button> */}
+        {!data && !error&& <h1>Loading..</h1>}
+        {error && <h1>{error.message}</h1>}
+        {data && <h1>Nome dell'utente : {data.name}</h1>}
+        {data && <h3>Followers : {data.followers}</h3>}
+        {data && <h3>Url GitHub{data.url}</h3>}
       </div>
     );
 }
