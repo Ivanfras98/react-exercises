a17c7ec8cf36c2a67a2426033e19c78ab2710e5b
Ivan <ivanfrasca92@gmail.com>
2022-09-09 12:18:39 +0200

    fix useconfigureStore

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,23 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import { combineReducers } from "redux";
 import { counterState } from "./CounterState";
-import { addTodo, editTodo, removeTodo, todoState } from "./TodoState";
+import { todoState } from "./TodoState";
 
 const rootReducer = combineReducers({
   counter: counterState.reducer,
   todo: todoState.reducer,
 });
-export const store = createStore(rootReducer);
+export const store = configureStore({ reducer: rootReducer });
 
-store.subscribe(()=> {
-  console.log(store.getState())
-})
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch(counterState.actions.increment(5));
-store.dispatch(todoState.actions.add({id : 1, title : "Lavare i piatti", completed : true}));
-store.dispatch(todoState.actions.edit({id : 1, title : "Cucinare", completed : true}));
- store.dispatch(todoState.actions.add({id : 2 , title : "Studiare", completed : true}));
- store.dispatch(todoState.actions.add({id : 3 , title : "Uscire", completed : false}));
-store.dispatch(todoState.actions.remove({id : 2}));
-store.dispatch(todoState.actions.remove({id : 3}));
-
+store.dispatch(
+  todoState.actions.add({ id: 1, title: "Lavare i piatti", completed: true })
+);
+store.dispatch(
+  todoState.actions.edit({ id: 1, title: "Cucinare", completed: true })
+);
+store.dispatch(
+  todoState.actions.add({ id: 2, title: "Studiare", completed: true })
+);
+store.dispatch(
+  todoState.actions.add({ id: 3, title: "Uscire", completed: false })
+);
+store.dispatch(todoState.actions.remove({ id: 2 }));
+store.dispatch(todoState.actions.remove({ id: 3 }));
